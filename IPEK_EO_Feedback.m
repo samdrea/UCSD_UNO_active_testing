81641fc6bf20295f5a0e7375570a0fbc2accef4b
Karl Johnson <k9johnso@ucsd.edu>
2024-07-11 11:53:27 -0700

    updated some output

--- IPEK_EO_Feedback.m
+++ IPEK_EO_Feedback.m
@@ -20,19 +20,25 @@ agilent_set_range(agi, powerMeterRange1, 1);
 
 % Set laser parameters
 laser_power_dbm = 4;
-laser_wavelength_nm = 1543.85; % Test different wavelengths to try to stabilize output
+laser_wavelength_nm = 1546.6; % max power for fixing paddles%1543.85; % Test different wavelengths to try to stabilize output
 venturi_set_power(ven, laser_power_dbm);
 venturi_set_wavelength(ven, laser_wavelength_nm);
 
 %% Turn laser on and fix paddles to get best alignment
 venturi_output(ven, true); % Turning the laser on
 
-%% Turn laser off after alignment
-%venturi_output(ven, false);
+%% Turn laser back to desired wavelength after alignment
+%venturi_output(ven, false); % Depends if i want the laser warm
+laser_wavelength_nm = 1543.85;
+venturi_set_wavelength(ven, laser_wavelength_nm);
 
 %% Calibration for a and b
+
+% Turn laser on
+venturi_output(ven, true); % Turning the laser on
+
 % Keithley voltage source sweep parameters
-v_min = 0; v_max = 4; v_step = 0.01; %0.01; 
+v_min = 0.5; v_max = 4; v_step = 0.01; %0.01; 
 v_comp = 5; % IPEK data sheet says 5.1374 V (heats to 30K) and 5.9321 V (heats to 50K)
 i_comp = 25; % mA. IPEK data sheet says 29.2 mA (heats to 30K) or 33.7 mA (heats to 50K)
 settle_time = 0.1; %0.001; %0.1; % seconds. For voltage to stabilize while I collect data?
@@ -52,7 +58,7 @@ agilent_results = [];
 % Turn laser off
 venturi_output(ven, false);
 
-% Get a and b
+%% Get a and b
 % Finding the voltage of lowest power as offset
 [min_value, index] = min(agilent_results); % Find the minimum value in agilent_results and its index
 V_offset = measured_V(index); % Should be around 0.7
@@ -61,22 +67,23 @@ V_offset = measured_V(index); % Should be around 0.7
 P_max = max(agilent_results);
 
 % Find the indices of values within the specified range in agilent_results
-P_ratio = 0.5; % How much of P_max we want our high-state voltage to be
+P_ratio = 0.6; % How much of P_max we want our high-state voltage to be
 lower_bound = P_max * P_ratio - 0.0005;
 upper_bound = P_max * P_ratio + 0.0005;
 possible_indices = find(agilent_results >= lower_bound & agilent_results <= upper_bound);
 
-% Finding the power and voltage of our "high-state"
-V_working = measured_V(possible_indices(1));
-P_working = agilent_results(possible_indices(1));
+% Finding the power and voltage of our "high-state" (the last will be the
+% one going downhill
+V_working = measured_V(possible_indices(end));
+P_working = agilent_results(possible_indices(end));
 
 % Calculating the constants
 a = asin(sqrt(P_working/P_max))/(V_working-V_offset);
 b = (V_working-V_offset)/P_working;
 
 % Create the formatted string
-output_str = sprintf('Calculated values: a = %.4f V^-1, b = %.4f V/mW, V_offset = %.4f V, P_working = %.4f mW, V_working = %.4f V', ...
-                     a, b, V_offset, P_working, V_working);
+output_str = sprintf('\nCalculated values:\na = %.4f V^-1\nb = %.4f V/mW \nP _max = %.4f mW \nP_working = %.4f mW \nV_offset = %.4f V \nV_working = %.4f V', ...
+                     a, b, P_max, P_working, V_offset, V_working);
 
 % Print the formatted string
 disp(output_str);
@@ -94,11 +101,11 @@ ylabel("Power (mW)");
 % Create arrays to collect data
 tuning_voltage_arr = []; % volts.
 measured_power_arr = []; % mW.
-%predicted_power_arr = []; % mW.
+predicted_power_arr = []; % mW.
 
 % Set feedback loop variables
-input_voltage = 0; % volts
-num_iter = 100;
+input_voltage = 3.55; % volts
+num_iter = 10;
 %P_max =  0.01354; % mW. Equal to our power input. Can probably use 1000 * 10 ** ((laser_power_dbm - 30) / 10)) 
 %a = 0.2322; % V^-1. Constant of proportionality for transmitted power  
 %b = 541.8719; % V/mW. Constant for proportionality for power to voltage conversion
@@ -129,8 +136,8 @@ for i = 1:num_iter
     % Collect output power
     curr_power = laser_get_power(agi); % Read in mW
     measured_power_arr = [measured_power_arr curr_power];
-    %predicted_power = P_max * (sin(a * input_voltage)) ^ 2;
-    %predicted_power_arr = [predicted_power_arr predicted_power];
+    predicted_power = P_max * (sin(a * input_voltage)) ^ 2;
+    predicted_power_arr = [predicted_power_arr predicted_power];
 
     % Let IPEK cool before next voltage
     key_set_V(key, 0);
@@ -159,16 +166,17 @@ venturi_output(ven, false);
 % Define an iteration array
 iterations = 1:num_iter;
 
+
 % Create a figure
 figure;
 
 % Plot the data with two y-axes
 yyaxis left
-plot(iterations, tuning_voltage_arr, '-o', 'Color', 'r', 'DisplayName', 'Tuning Voltage');
+plot(iterations, tuning_voltage_arr, '-o', 'Color', 'b', 'DisplayName', 'Tuning Voltage');
 ylabel('Voltage (V)');
 
 yyaxis right
-plot(iterations, measured_power_arr, '-x', 'Color', 'b', 'DisplayName', 'Measured Power');
+plot(iterations, measured_power_arr, '-x', 'Color', 'r', 'DisplayName', 'Measured Power');
 ylabel('Power (mW)');
 
 % Add labels and title
@@ -181,10 +189,10 @@ grid on;
 annotation_str = sprintf(['Calculated values:\n' ...
                           'a = %.4f\n' ...
                           'b = %.4f\n' ...
-                          'P_max = %.4f mW\n' ...
-                          'P_working = %.4f mW\n' ...
-                          'V_working = %.4f V\n' ...
-                          'V_offset = %.4f V\n' ...
+                          'P_{max} = %.4f mW\n' ...
+                          'P_{working} = %.4f mW\n' ...
+                          'V_{working} = %.4f V\n' ...
+                          'V_{offset} = %.4f V\n' ...
                           'Wavelength = %.4f nm\n' ...
                           'Power Input = %.4f mW'], ...
                           a, b, P_max, P_working, V_working, V_offset, laser_wavelength_nm, 10 * log10(laser_power_dbm));
@@ -202,7 +210,7 @@ plot(iterations, measured_power_arr, '-x', 'Color', 'b', 'DisplayName', 'Measure
 hold on; % Hold the current plot
 
 % Plot the predicted power against iterations
-%plot(iterations, predicted_power_arr, '-o', 'Color', 'r', 'DisplayName', 'Predicted Power'); 
+plot(iterations, predicted_power_arr, '-o', 'Color', 'r', 'DisplayName', 'Predicted Power'); 
 
 % Add labels and title
 xlabel('Iteration');
@@ -220,7 +228,7 @@ plot(tuning_voltage_arr, measured_power_arr, '-x', 'Color', 'b', 'DisplayName', 
 hold on; % Hold the current plot
 
 % Plot the predicted power
-%plot(tuning_voltage_arr, predicted_power_arr, '-o', 'Color', 'r', 'DisplayName', 'Predicted Power'); 
+plot(tuning_voltage_arr, predicted_power_arr, '-o', 'Color', 'r', 'DisplayName', 'Predicted Power'); 
 
 % Add labels and title
 xlabel('Voltage (V)');
